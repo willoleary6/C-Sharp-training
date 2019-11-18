d9e1c81e904f15462d9e0c310e655f1736cb4d1b
William <willoleary6@gmail.com>
2019-11-18 20:03:43 +0000

    pressing on with practice as I have the free time to do so

---
+++ recursion_natural_number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace recursion_natural_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            // Natural number starting point
+            int starting_print_val = 1;
+            // n
+            int target_val = 10;
+            // begin recursion
+            Natural_numbers_recursion(starting_print_val, target_val);
+        }
+
+        static int Natural_numbers_recursion(int starting_print_val, int target_val)
+        {
+            // dont want it to sink below natural number boundry
+            if( target_val < 1)
+            {
+                // fall out of recursive loop
+                return 0;
+            }
+            // decrementor of loop
+            target_val--;
+            Console.Write(" {0} ",starting_print_val);
+
+            starting_print_val++;
+            return Natural_numbers_recursion(starting_print_val, target_val);
+        }
+    }
+}
---
+++ shell_short/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace shell_short
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sort_me = { 2340, 4523, 3421 };
+            // calculate interval
+            int interval = 0;
+            while(interval < sort_me.Length)
+            {
+                interval = interval * 3 + 1;
+            }
+
+            while(interval > 0)
+            {
+                for(int outer = interval; outer < sort_me.Length; outer++)
+                {
+                    // value to be inserted
+                    int valueToInsert = sort_me[outer];
+                    int inner = outer;
+
+                    // shift element to the write
+                    while(inner > interval -1 && sort_me[inner - interval] >= valueToInsert)
+                    {
+                        sort_me[inner] = sort_me[inner - interval];
+                        inner -= interval;
+                    }
+                    // insert value on hole position
+                    sort_me[inner] = valueToInsert;
+                }
+                // recalculate interval
+                interval = (interval - 1) / 3;
+            }
+
+           for(int i = 0; i < sort_me.Length; i++)
+           {
+                Console.WriteLine(sort_me[i]);
+           }
+            
+        }
+    }
+}
